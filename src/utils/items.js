2c07b45a72efc7944d7d42c38ad07d3734f1c573
Gonzalo Cercos <Gonxis@users.noreply.github.com>
2020-07-03 18:57:47 +0200

    Create items.js

---
+++ src/utils/items.js
@@ -0,0 +1,22 @@
+[
+{
+        "Id": "001",
+        "Likes": 6,
+        "Title": "Headphones"
+    },
+    {
+        "Id": "002",
+        "Likes": 100,
+        "Title": "TV"
+    },
+    {
+        "Id": "050",
+        "Likes": 0,
+        "Title": "Mobile"
+    },
+    {
+        "Id": 169,
+        "Likes": 0,
+        "Title": "Speakers"
+    }
+]
